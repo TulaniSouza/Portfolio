cd275a285ff41202fceab7e6e42ce3bb03dab337
Tulani <tulanidepaula@gmail.com>
2024-04-04 11:09:36 -0300

    fazendo o menu

--- src/Navegacao/Navegacao.jsx
+++ src/Navegacao/Navegacao.jsx
@@ -8,35 +8,69 @@ import Logo from"../assets/Logotipo.png"
 import Github from "../assets/github.png"
 import Whatsapp from "../assets/whatsapp.jpg"
 
-
 function Navegacao(){
-      
+
+    ItemMenu.forEach((item) => (item.classList.remove('ativo')))
+    this.classList.add('ativo')
+
+    menuItem.forEach((item) => (item.addEventListener('click', selectLink)))
+
+    //Expandir o menu
+
+    let btnExp = document.querySelector('#btn-exp')
+    let menuSide = document.querySelector('S.Nav')
+
+    btnExp.addEventListener('click'), function(){
+    menuSide.classList.toggle('expandir')}
+
     return(
-        <BrowserRouter>
+       <BrowserRouter>
         
-         <S.Nav> 
-            <S.Image src={Logo} alt="Logo" title="LogoTC"/>        
-            <S.Lista>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/About">About</Link></li>
-                <li><Link to="/Projects">Projects</Link></li>
-            </S.Lista>
+            <S.Nav> 
+                <S.Image src={Logo} alt="Logo" title="LogoTC"/>        
+                <S.Lista>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/About">About</Link></li>
+                    <li><Link to="/Projects">Projects</Link></li>
+                </S.Lista>
             
-            <S.Screen src={Github} alt="Github" title="Git conheca +"/>
+                <S.Screen src={Github} alt="Github" title="Git conheca +"/>
             
-            <S.Photo src={Whatsapp} alt="Whatsapp" title="Contato"/>
-           
-        </S.Nav>
-        
-        <Routes>
-            <Route path="/" element={ <Home /> }/>
-            <Route pathh="About" element={ <About />}/>
-            <Route path="Projects" element= { <Projects /> }/>
-        </Routes>
+                <S.Photo src={Whatsapp} alt="Whatsapp" title="Contato"/>
+                <S.Divisao>
+                    <i class="bi bi-list" id="btn-exp"></i>
+                </S.Divisao>
+
+                <S.Listagem>
+                    <S.ItemMenu>
+                        <a href="#">
+                            <span class="icon"><i class="bi bi-house-door"></i></span>
+                            <span class="txt-link">Home</span>
+                        </a>
+                    </S.ItemMenu>
+                    <S.ItemMenu1>
+                        <a href="#">
+                            <span class="icon"><i class="bi bi-columns-gap"></i></span>
+                            <span class="txt-link">About</span>
+                        </a>
+                    </S.ItemMenu1>
+                    <S.ItemMenuDois>
+                        <a href="#">
+                            <span class="icon"><i class="bi bi-person-circle"></i></span>
+                            <span class="txt-link">Projects</span>
+                        </a>
+                    </S.ItemMenuDois>
+                </S.Listagem>
+       
+            </S.Nav>
+
+            <Routes>
+                <Route path="/" element={ <Home /> }/>
+                <Route pathh="About" element={ <About />}/>
+                <Route path="Projects" element= { <Projects /> }/>
+            </Routes>
 
         </BrowserRouter>
-    )
-}
-
+)   }
 
 export default Navegacao
--- src/Navegacao/style.jsx
+++ src/Navegacao/style.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const Image= styled.img`
 display: flex;
 border-radius: 60%;
@@ -9,11 +8,24 @@ justify-content: center;
 
 `
 export const Nav= styled.nav`
-width: 100%;
-display: flex;
-align-items: center;
-justify-content: space-around;
-text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    text-decoration: none;
+    width: 80px;
+    height: 100%;
+    background-color: #202020;
+    padding: 40px 0 40px 1%;
+    box-shadow: 3px 0 0 #FF0077;
+    position: fixed;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    transition: .5s;
+`
+export const Nave =styled.nav`
+   width: 300px;
+
 `
 
 export const Lista= styled.ul`
@@ -40,3 +52,51 @@ width: 8%;
 cursor: pointer;
 
 `
+
+export const btn=styled.div`
+width: 100%;
+padding-left: 10px;
+`
+
+export const button= styled.div`
+    color: #fff;
+    font-size: 24px;
+    cursor: pointer;
+`
+
+export const Listagem= styled.ul`
+height: 100%;
+list-style-type: none;
+`
+    
+export const ItemMenu= styled.li`
+transition: .2s;
+`
+export const ItemMenu1= styled.li`
+margin-left: 70px;
+    transition: .5s;
+    opacity: 0;
+
+`
+export const ItemMenuDois= styled.li`
+margin-left: 40px;
+opacity: 1;
+`
+
+export const ListUl= styled.li`
+background-color: #FF0077;
+`
+export const ItemDois= styled.li`
+background: #FF0077;
+`
+
+export const List=styled.li`
+color: #fff;
+    text-decoration: none;
+    font-size: 20px;
+    padding: 20px 4%;
+    display: flex;
+    margin-bottom: 20px;
+    line-height: 40px;
+` 
+
